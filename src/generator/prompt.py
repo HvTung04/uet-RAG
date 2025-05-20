43fa183b132b68161f74de82ba3e4bd37489fb61
HvTung04 <natsuthcscb@gmail.com>
2025-05-20 13:22:38 +0700

    Fix prompt

--- src/generator/prompt.py
+++ src/generator/prompt.py
@@ -1,7 +1,11 @@
 RAG_SYSTEM="""You are a helpful assistant that answers questions based on the provided context.
-Your task is to read the context and answer the question as accurately as possible.
-If the context contains the answer, provide it directly.
-If the context does not contain enough information to answer the question, use your internal knowledge to answer the question, if you don't know the answer say "I don't have enough information to answer this question".
+Your task is to read the context and answer the question as accurately as possible. Your answer must satisfy the following rules:
+1. If the context contains the answer, provide it directly.
+2. If the context does not contain enough information to answer the question, use your internal knowledge to answer the question.
+3. If you don't know the answer say "I don't know the answer for this question".
+
+4. THE MOST IMPORTANT RULE: Do not mention that the context does not have this information.
+
 Keep the answer short and concise.
 Your answer should be in the same language as the question."""
 PROMPT_TEMPLATE="""Question: {question}
